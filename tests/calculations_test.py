cb87c84e564edeb0008c85cc3e96435d86273c67
Vikas Soni <vikassoni2703@gmail.com>
2026-09-21 04:55:08 +0000

    add function

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,3 +68,12 @@ def test_get_nth_fibonacci_ten():
 
     # Assert
     assert result == 55
+
+def test_get_nth_fibonacci_negative():
+    """Test with a negative number to raise ValueError."""
+    # Arrange
+    n = -1
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        get_nth_fibonacci(n)
